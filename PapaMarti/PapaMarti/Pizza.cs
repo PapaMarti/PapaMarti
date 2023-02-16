7b06ef55f458df6b14d5a994d98ba39f4059b7cb
KaranSharma727 <sharma.karany727@gmail.com>
2023-02-16 11:20:52 -0600

    added pizza class

---
+++ PapaMarti/PapaMarti/Pizza.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+enum Toppings
+{
+    tomatoSauce,
+    cheese,
+    pepperoni,
+    sausage,
+    olives,
+    onions,
+    chicken,
+    tomato,
+    jalapeno
+}
+
+namespace PapaMarti
+{
+    class Pizza
+    {
+        List<Rectangle> outline;
+        List<Toppings> toppings;
+        int cookTime;
+
+        /// <summary>
+        /// Creates a pizza that holds its specific values.
+        /// </summary>
+        /// <param name="cookTime">The amount of time it takes to cook this pizza.</param>
+        /// <param name="toppings">The types of toppings that this pizza will have.</param>
+        /// <param name="outline">List of Rectangles outlining where the pizza is supposed to be cut</param>
+        public Pizza(List<Rectangle> outline, List<Toppings> toppings, int cookTime)
+        {
+            this.outline = outline;
+            this.toppings = toppings;
+            this.cookTime = cookTime;
+        }
+    }
+}
