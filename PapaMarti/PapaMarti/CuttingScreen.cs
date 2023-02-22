9fce963fe2a46ec460bd9e752338273966eff007
Jacob Baumel <jacob.a.baumel@gmail.com>
2023-02-22 10:54:40 -0600

    Fixed Casting errors

--- PapaMarti/PapaMarti/CuttingScreen.cs
+++ PapaMarti/PapaMarti/CuttingScreen.cs
@@ -72,7 +72,7 @@ namespace PapaMarti
                 for(int i = 0; i < mouseLocations.Count; i++)
                 {
                     //makes the color anywhere from green to red depending on how accurate the point is
-                    Color accuracyColor = new Color((float)(1.0 - accuracies[i]), (float)accuracies[i], 0.0);
+                    Color accuracyColor = new Color((float)(1.0 - accuracies[i]), (float)accuracies[i], 0.0f);
                     pixel.X = mouseLocations[i].X;
                     pixel.Y = mouseLocations[i].Y;
                     spriteBatch.Draw(dough, pixel, accuracyColor);
@@ -100,7 +100,7 @@ namespace PapaMarti
                         step *= -1;
                     double xStep = (double)xDiff / diff;
                     double yStep = (double)yDiff / diff;
-                    for(int i = step, ; Math.Abs(i) < Math.Abs(diff); i+=step){
+                    for(int i = step; Math.Abs(i) < Math.Abs(diff); i+=step){
                         mouseLocations.Add(new Point(previous.X + (int)(i * xStep), previous.Y + (int)(i * yStep)));
                     }
                 }
@@ -158,61 +158,61 @@ namespace PapaMarti
                 //i dont remember if these need to be ints or doubles so if they need to be int just cast them but after double calculations have been done
                 //i hate rectangles
                 // X: 9 to 15, Y: 1
-                rects.Add(new Rectangle(oX + (8.0 * (oW / 23.0)), oY, 7.0 * (oW / 23.0), oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (8.0 * (oW / 23.0))), oY, (int) (7.0 * (oW / 23.0)), (int) (oH / 23.0)));
                 // X: 7 to 8, Y: 2
-                rects.Add(new Rectangle(oX + (6.0 * (oW / 23.0)), oY + (oH / 23.0), 2.0 * (oW / 23.0), oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (6.0 * (oW / 23.0))), (int) (oY + (oH / 23.0)), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
                 // X: 16 to 17, Y: 2
-                rects.Add(new Rectangle(oX + (15.0 * (oW / 23.0)), oY + (oH / 23.0), 2.0 * (oW / 23.0), oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (15.0 * (oW / 23.0))), (int) (oY + (oH / 23.0)), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
                 // X: 6, Y: 3
-                rects.Add(new Rectangle(oX + (5.0 * (oW / 23.0)), oY + (2.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (5.0 * (oW / 23.0))), (int) (oY + (2.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 18, Y: 3
-                rects.Add(new Rectangle(oX + (17.0 * (oW / 23.0)), oY + (2.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (17.0 * (oW / 23.0))), (int) (oY + (2.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 4 to 5, Y: 4
-                rects.Add(new Rectangle(oX + (3.0 * (oW / 23.0)), oY + (3.0 * (oH / 23.0)), 2.0 * (oW / 23.0), oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (3.0 * (oW / 23.0))), (int) (oY + (3.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
                 // X: 19 to 20, Y: 4
-                rects.Add(new Rectangle(oX + (18.0 * (oW / 23.0)), oY + (3.0 * (oH / 23.0)), 2.0 * (oW / 23.0), oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (18.0 * (oW / 23.0))), (int) (oY + (3.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
                 // X: 4, Y: 5
-                rects.Add(new Rectangle(oX + (3.0 * (oW / 23.0)), oY + (4.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (3.0 * (oW / 23.0))), (int) (oY + (4.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 20, Y: 5
-                rects.Add(new Rectangle(oX + (19.0 * (oW / 23.0)), oY + (4.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (19.0 * (oW / 23.0))), (int) (oY + (4.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 3, Y: 6
-                rects.Add(new Rectangle(oX + (2.0 * (oW / 23.0)), oY + (5.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (2.0 * (oW / 23.0))), (int) (oY + (5.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 21, Y: 6
-                rects.Add(new Rectangle(oX + (20.0 * (oW / 23.0)), oY + (5.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (20.0 * (oW / 23.0))), (int) (oY + (5.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 2, Y: 7 to 8
-                rects.Add(new Rectangle(oX + (oW / 23.0), oY + (6.0 * (oH / 23.0)), oW / 23.0, 2.0 * (oH / 23.0)));
+                rects.Add(new Rectangle((int) (oX + (oW / 23.0)), (int) (oY + (6.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (2.0 * (oH / 23.0))));
                 // X: 22, Y: 7 to 8
-                rects.Add(new Rectangle(oX + (21.0 * (oW / 23.0)), oY + (6.0 * (oH / 23.0)), oW / 23.0, 2.0 * (oH / 23.0)));
+                rects.Add(new Rectangle((int) (oX + (21.0 * (oW / 23.0))), (int) (oY + (6.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (2.0 * (oH / 23.0))));
                 // X: 1, Y: 9 to 15
-                rects.Add(new Rectangle(oX, oY + (8.0 * (oH / 23.0)), oW / 23.0, 7.0 * (oH / 23.0)));
+                rects.Add(new Rectangle((int) (oX), (int) (oY + (8.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (7.0 * (oH / 23.0))));
                 // X: 23, Y: 9 to 15
-                rects.Add(new Rectangle(oX + (22.0 * (oW / 23.0)), oY + (8.0 * (oH / 23.0)), oW / 23.0, 7.0 * (oH / 23.0)));
+                rects.Add(new Rectangle((int) (oX + (22.0 * (oW / 23.0))), (int) (oY + (8.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (7.0 * (oH / 23.0))));
                 // X: 2, Y: 16 to 17
-                rects.Add(new Rectangle(oX + (oW / 23.0), oY + (15.0 * (oH / 23.0)), oW / 23.0, 2.0 * (oH / 23.0)));
+                rects.Add(new Rectangle((int) (oX + (oW / 23.0)), (int) (oY + (15.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (2.0 * (oH / 23.0))));
                 // X: 22, Y: 16 to 17
-                rects.Add(new Rectangle(oX + (21.0 * (oW / 23.0)), oY + (15.0 * (oH / 23.0)), oW / 23.0, 2.0 * (oH / 23.0)));
+                rects.Add(new Rectangle((int) (oX + (21.0 * (oW / 23.0))), (int) (oY + (15.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (2.0 * (oH / 23.0))));
                 // X: 3, Y: 18
-                rects.Add(new Rectangle(oX + (2.0 * (oW / 23.0)), oY + (17.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (2.0 * (oW / 23.0))), (int) (oY + (17.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 21, Y: 18
-                rects.Add(new Rectangle(oX + (20.0 * (oW / 23.0)), oY + (17.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (20.0 * (oW / 23.0))), (int) (oY + (17.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 4, Y: 19
-                rects.Add(new Rectangle(oX + (3.0 * (oW / 23.0)), oY + (18.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (3.0 * (oW / 23.0))), (int) (oY + (18.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 20, Y: 19
-                rects.Add(new Rectangle(oX + (19.0 * (oW / 23.0)), oY + (18.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (19.0 * (oW / 23.0))), (int) (oY + (18.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 4 to 5, Y: 20
-                rects.Add(new Rectangle(oX + (3.0 * (oW / 23.0)), oY + (19.0 * (oH / 23.0)), 2.0 * (oW / 23.0), oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (3.0 * (oW / 23.0))), (int) (oY + (19.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
                 // X: 19 to 20, Y: 20
-                rects.Add(new Rectangle(oX + (18.0 * (oW / 23.0)), oY + (19.0 * (oH / 23.0)), 2.0 * (oW / 23.0), oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (18.0 * (oW / 23.0))), (int) (oY + (19.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
                 // X: 6, Y: 21
-                rects.Add(new Rectangle(oX + (5.0 * (oW / 23.0)), oY + (20.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (5.0 * (oW / 23.0))), (int) (oY + (20.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 18, Y: 21
-                rects.Add(new Rectangle(oX + (17.0 * (oW / 23.0)), oY + (20.0 * (oH / 23.0)), oW / 23.0, oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (17.0 * (oW / 23.0))), (int) (oY + (20.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
                 // X: 7 to 8, Y: 22
-                rects.Add(new Rectangle(oX + (6.0 * (oW / 23.0)), oY + (21.0 * (oH / 23.0)), 2.0 * (oW / 23.0), oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (6.0 * (oW / 23.0))), (int) (oY + (21.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
                 // X: 16 to 17, Y: 22
-                rects.Add(new Rectangle(oX + (15.0 * (oW / 23.0)), oY + (21.0 * (oH / 23.0)), 2.0 * (oW / 23.0), oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (15.0 * (oW / 23.0))), (int) (oY + (21.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
                 // X: 9 to 15, Y: 23
-                rects.Add(new Rectangle(oX + (8.0 * (oW / 23.0)), oY + (22.0 * (oH / 23.0)), 7.0 * (oW / 23.0), oH / 23.0));
+                rects.Add(new Rectangle((int) (oX + (8.0 * (oW / 23.0))), (int) (oY + (22.0 * (oH / 23.0))), (int) (7.0 * (oW / 23.0)), (int) (oH / 23.0)));
             }
             return rects;
         }
