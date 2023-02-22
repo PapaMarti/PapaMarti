5123f47f83966ae48c6af6d4e3831f2eddc0cf39
ella-whhh <125389981+ella-whhh@users.noreply.github.com>
2023-02-22 14:05:43 -0600

    fixed coloring of mouse trail

--- PapaMarti/PapaMarti/CuttingScreen.cs
+++ PapaMarti/PapaMarti/CuttingScreen.cs
@@ -57,25 +57,39 @@ namespace PapaMarti
             accuracies = new List<double>();
             mouseLocations = new List<Point>();
             outlineRects = getOutlineRects();
+
+            //for testing delete later
+            //for(int r = 0; r < tableRect.Width; r++)
+            //{
+            //    for(int c = tableRect.Y; c < tableRect.Height + tableRect.Y; c++)
+            //    {
+            //        mouseLocations.Add(new Point(r, c));
+            //        accuracies.Add(getMousePointAccuracy(mouseLocations[mouseLocations.Count - 1]));
+            //    }
+            //}
         }
 
         public void draw(SpriteBatch spriteBatch)
         {
             //table
             spriteBatch.Draw(whitePixel, tableRect, Color.Gray);
-            if(done){
+            if (done)
+            {
                 spriteBatch.Draw(cutDough, outlineTextRect, Color.White);
-            }else{
+            }
+            else
+            {
                 spriteBatch.Draw(dough, uncutDoughRect, Color.White);
                 spriteBatch.Draw(outline, outlineTextRect, Color.White);
 
-                //drawing rectangles used for accuracy
-                for(int i = 0; i < rects; i++){
-                    spriteBatch.Draw(whitePixel, outlineRects[i], Color.Red);
-                }
+                //testing by drawing rectangles used for accuracy
+                //for (int i = 0; i < outlineRects.Count; i++)
+                //{
+                //    spriteBatch.Draw(whitePixel, outlineRects[i], Color.Red);
+                //}
 
                 Rectangle pixel = new Rectangle(0, 0, 2, 2);
-                for(int i = 0; i < mouseLocations.Count; i++)
+                for (int i = 0; i < mouseLocations.Count; i++)
                 {
                     //makes the color anywhere from green to red depending on how accurate the point is
                     Color accuracyColor = new Color((float)(1.0 - accuracies[i]), (float)accuracies[i], 0.0f);
@@ -89,40 +103,34 @@ namespace PapaMarti
         public void update(GameTime time)
         {
             MouseState mouse = Mouse.GetState();
-            if(mouse.LeftButton == ButtonState.Pressed && !done)
+            if (mouse.LeftButton == ButtonState.Pressed && !done)
             {
                 Point mousePosition = new Point(mouse.X, mouse.Y);
                 //the point of this whole if statement is to prevent gaps in the line it draws as the mouse moves
                 int previousIndex = mouseLocations.Count - 1;
-                if(mouseIsPressed){
+                if (mouseIsPressed)
+                {
                     Point previous = mouseLocations[previousIndex];
                     int xDiff = mousePosition.X - previous.X;
                     int yDiff = mousePosition.Y - previous.Y;
                     int diff = yDiff;
-                    if(Math.Abs(xDiff) > Math.Abs(yDiff))
+                    if (Math.Abs(xDiff) > Math.Abs(yDiff))
                         diff = xDiff;
                     int step = 2;
-                    if(diff < 0)
+                    if (diff < 0)
                         step *= -1;
                     double xStep = (double)xDiff / diff;
                     double yStep = (double)yDiff / diff;
-                    for(int i = step; Math.Abs(i) < Math.Abs(diff); i+=step){
+                    for (int i = step; Math.Abs(i) < Math.Abs(diff); i += step)
+                    {
                         mouseLocations.Add(new Point(previous.X + (int)(i * xStep), previous.Y + (int)(i * yStep)));
+                        accuracies.Add(getMousePointAccuracy(mouseLocations[mouseLocations.Count - 1]));
                     }
                 }
                 mouseLocations.Add(mousePosition);
-                int currentIndex = mouseLocations.Count - 1;
-                //adding accuracy of all the points added
-                if(previousIndex >= 0) {
-                    for(int i = previousIndex; i <= currentIndex; i++) {
-                        accuracies.Add(getMousePointAccuracy(mouseLocations[i]));
-                    }
-                }
-                else {
-                    accuracies.Add(getMousePointAccuracy(mouseLocations[0]));
-                }
+                accuracies.Add(getMousePointAccuracy(mouseLocations[mouseLocations.Count - 1]));
                 //checking if done, not done
-                Point first = mouseLocations[0];
+                //Point first = mouseLocations[0];
                 //for(int i = 1; i < mouseLocations.Count; i++){
                 //    if(Math.Abs(first.X - mouseLocations[i].X) < 4 && Math.Abs(first.Y - mouseLocations[i].Y) < 4){
                 //        done = true;
@@ -134,7 +142,8 @@ namespace PapaMarti
             else if (mouseIsPressed)
             {
                 mouseIsPressed = false;
-                if(!done){
+                if (!done)
+                {
                     mouseLocations = new List<Point>();
                     accuracies = new List<double>();
                 }
@@ -149,7 +158,7 @@ namespace PapaMarti
             if (accuracies.Count == 0)
                 return 0.0;
             double average = 0.0;
-            for(int i = 0; i < accuracies.Count; i++)
+            for (int i = 0; i < accuracies.Count; i++)
             {
                 average += accuracies[i];
             }
@@ -159,7 +168,7 @@ namespace PapaMarti
         {
             List<Rectangle> rects = new List<Rectangle>();
             //for now this is an enum i force you to give me in the constructor
-            if(shape == PizzaShape.Circle)
+            if (shape == PizzaShape.Circle)
             {
                 int oX = outlineTextRect.X;
                 int oY = outlineTextRect.Y;
@@ -169,63 +178,64 @@ namespace PapaMarti
                 //i dont remember if these need to be ints or doubles so if they need to be int just cast them but after double calculations have been done
                 //i hate rectangles
                 // X: 9 to 15, Y: 1
-                rects.Add(new Rectangle((int) (oX + (8.0 * (oW / 23.0))), oY, (int) (7.0 * (oW / 23.0)), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (8.0 * (oW / 23.0))), oY, (int)(7.0 * (oW / 23.0)), (int)(oH / 23.0)));
                 // X: 7 to 8, Y: 2
-                rects.Add(new Rectangle((int) (oX + (6.0 * (oW / 23.0))), (int) (oY + (oH / 23.0)), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (6.0 * (oW / 23.0))), (int)(oY + (oH / 23.0)), (int)(2.0 * (oW / 23.0)), (int)(oH / 23.0)));
                 // X: 16 to 17, Y: 2
-                rects.Add(new Rectangle((int) (oX + (15.0 * (oW / 23.0))), (int) (oY + (oH / 23.0)), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (15.0 * (oW / 23.0))), (int)(oY + (oH / 23.0)), (int)(2.0 * (oW / 23.0)), (int)(oH / 23.0)));
                 // X: 6, Y: 3
-                rects.Add(new Rectangle((int) (oX + (5.0 * (oW / 23.0))), (int) (oY + (2.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (5.0 * (oW / 23.0))), (int)(oY + (2.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 18, Y: 3
-                rects.Add(new Rectangle((int) (oX + (17.0 * (oW / 23.0))), (int) (oY + (2.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (17.0 * (oW / 23.0))), (int)(oY + (2.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 4 to 5, Y: 4
-                rects.Add(new Rectangle((int) (oX + (3.0 * (oW / 23.0))), (int) (oY + (3.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (3.0 * (oW / 23.0))), (int)(oY + (3.0 * (oH / 23.0))), (int)(2.0 * (oW / 23.0)), (int)(oH / 23.0)));
                 // X: 19 to 20, Y: 4
-                rects.Add(new Rectangle((int) (oX + (18.0 * (oW / 23.0))), (int) (oY + (3.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (18.0 * (oW / 23.0))), (int)(oY + (3.0 * (oH / 23.0))), (int)(2.0 * (oW / 23.0)), (int)(oH / 23.0)));
                 // X: 4, Y: 5
-                rects.Add(new Rectangle((int) (oX + (3.0 * (oW / 23.0))), (int) (oY + (4.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (3.0 * (oW / 23.0))), (int)(oY + (4.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 20, Y: 5
-                rects.Add(new Rectangle((int) (oX + (19.0 * (oW / 23.0))), (int) (oY + (4.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (19.0 * (oW / 23.0))), (int)(oY + (4.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 3, Y: 6
-                rects.Add(new Rectangle((int) (oX + (2.0 * (oW / 23.0))), (int) (oY + (5.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (2.0 * (oW / 23.0))), (int)(oY + (5.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 21, Y: 6
-                rects.Add(new Rectangle((int) (oX + (20.0 * (oW / 23.0))), (int) (oY + (5.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (20.0 * (oW / 23.0))), (int)(oY + (5.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 2, Y: 7 to 8
-                rects.Add(new Rectangle((int) (oX + (oW / 23.0)), (int) (oY + (6.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (2.0 * (oH / 23.0))));
+                rects.Add(new Rectangle((int)(oX + (oW / 23.0)), (int)(oY + (6.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(2.0 * (oH / 23.0))));
                 // X: 22, Y: 7 to 8
-                rects.Add(new Rectangle((int) (oX + (21.0 * (oW / 23.0))), (int) (oY + (6.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (2.0 * (oH / 23.0))));
+                rects.Add(new Rectangle((int)(oX + (21.0 * (oW / 23.0))), (int)(oY + (6.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(2.0 * (oH / 23.0))));
                 // X: 1, Y: 9 to 15
-                rects.Add(new Rectangle((int) (oX), (int) (oY + (8.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (7.0 * (oH / 23.0))));
+                rects.Add(new Rectangle((int)(oX), (int)(oY + (8.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(7.0 * (oH / 23.0))));
                 // X: 23, Y: 9 to 15
-                rects.Add(new Rectangle((int) (oX + (22.0 * (oW / 23.0))), (int) (oY + (8.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (7.0 * (oH / 23.0))));
+                rects.Add(new Rectangle((int)(oX + (22.0 * (oW / 23.0))), (int)(oY + (8.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(7.0 * (oH / 23.0))));
                 // X: 2, Y: 16 to 17
-                rects.Add(new Rectangle((int) (oX + (oW / 23.0)), (int) (oY + (15.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (2.0 * (oH / 23.0))));
+                rects.Add(new Rectangle((int)(oX + (oW / 23.0)), (int)(oY + (15.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(2.0 * (oH / 23.0))));
                 // X: 22, Y: 16 to 17
-                rects.Add(new Rectangle((int) (oX + (21.0 * (oW / 23.0))), (int) (oY + (15.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (2.0 * (oH / 23.0))));
+                rects.Add(new Rectangle((int)(oX + (21.0 * (oW / 23.0))), (int)(oY + (15.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(2.0 * (oH / 23.0))));
                 // X: 3, Y: 18
-                rects.Add(new Rectangle((int) (oX + (2.0 * (oW / 23.0))), (int) (oY + (17.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (2.0 * (oW / 23.0))), (int)(oY + (17.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 21, Y: 18
-                rects.Add(new Rectangle((int) (oX + (20.0 * (oW / 23.0))), (int) (oY + (17.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (20.0 * (oW / 23.0))), (int)(oY + (17.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 4, Y: 19
-                rects.Add(new Rectangle((int) (oX + (3.0 * (oW / 23.0))), (int) (oY + (18.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (3.0 * (oW / 23.0))), (int)(oY + (18.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 20, Y: 19
-                rects.Add(new Rectangle((int) (oX + (19.0 * (oW / 23.0))), (int) (oY + (18.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (19.0 * (oW / 23.0))), (int)(oY + (18.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 4 to 5, Y: 20
-                rects.Add(new Rectangle((int) (oX + (3.0 * (oW / 23.0))), (int) (oY + (19.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (3.0 * (oW / 23.0))), (int)(oY + (19.0 * (oH / 23.0))), (int)(2.0 * (oW / 23.0)), (int)(oH / 23.0)));
                 // X: 19 to 20, Y: 20
-                rects.Add(new Rectangle((int) (oX + (18.0 * (oW / 23.0))), (int) (oY + (19.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (18.0 * (oW / 23.0))), (int)(oY + (19.0 * (oH / 23.0))), (int)(2.0 * (oW / 23.0)), (int)(oH / 23.0)));
                 // X: 6, Y: 21
-                rects.Add(new Rectangle((int) (oX + (5.0 * (oW / 23.0))), (int) (oY + (20.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (5.0 * (oW / 23.0))), (int)(oY + (20.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 18, Y: 21
-                rects.Add(new Rectangle((int) (oX + (17.0 * (oW / 23.0))), (int) (oY + (20.0 * (oH / 23.0))), (int) (oW / 23.0), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (17.0 * (oW / 23.0))), (int)(oY + (20.0 * (oH / 23.0))), (int)(oW / 23.0), (int)(oH / 23.0)));
                 // X: 7 to 8, Y: 22
-                rects.Add(new Rectangle((int) (oX + (6.0 * (oW / 23.0))), (int) (oY + (21.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (6.0 * (oW / 23.0))), (int)(oY + (21.0 * (oH / 23.0))), (int)(2.0 * (oW / 23.0)), (int)(oH / 23.0)));
                 // X: 16 to 17, Y: 22
-                rects.Add(new Rectangle((int) (oX + (15.0 * (oW / 23.0))), (int) (oY + (21.0 * (oH / 23.0))), (int) (2.0 * (oW / 23.0)), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (15.0 * (oW / 23.0))), (int)(oY + (21.0 * (oH / 23.0))), (int)(2.0 * (oW / 23.0)), (int)(oH / 23.0)));
                 // X: 9 to 15, Y: 23
-                rects.Add(new Rectangle((int) (oX + (8.0 * (oW / 23.0))), (int) (oY + (22.0 * (oH / 23.0))), (int) (7.0 * (oW / 23.0)), (int) (oH / 23.0)));
+                rects.Add(new Rectangle((int)(oX + (8.0 * (oW / 23.0))), (int)(oY + (22.0 * (oH / 23.0))), (int)(7.0 * (oW / 23.0)), (int)(oH / 23.0)));
             }
-            else{ //if we add more shapes the rectangles for those can go here, just change this to else if
+            else
+            { //if we add more shapes the rectangles for those can go here, just change this to else if
 
             }
             return rects;
@@ -233,40 +243,53 @@ namespace PapaMarti
         private double getMousePointAccuracy(Point point)
         {
             double bestAccuracy = 0;
-            int buffer = 10;
-            for(int i = 0; i < outlineRects.Count; i++)
+            int buffer = 3;
+            for (int i = 0; i < outlineRects.Count; i++)
             {
                 double currentAccuracy = 0;
                 double xDistance, yDistance;
-                if(point.X <= outlineRects[i].Width + outlineRects[i].X + buffer && point.X >= outlineRects[i].X - buffer && point.Y >= outlineRects[i].Y - buffer && point.Y <= outlineRects[i].Height + outlineRects[i].Y + buffer)
+                if (point.X <= outlineRects[i].Width + outlineRects[i].X + buffer && point.X >= outlineRects[i].X - buffer && point.Y >= outlineRects[i].Y - buffer && point.Y <= outlineRects[i].Height + outlineRects[i].Y + buffer)
                 {
                     bestAccuracy = 1.0;
                     break;
                 }
-                if(Math.Abs(outlineRects[i].Width + outlineRects[i].X - point.X) < Math.Abs(outlineRects[i].X - point.X))
+                if (Math.Abs(outlineRects[i].Width + outlineRects[i].X - point.X) < Math.Abs(outlineRects[i].X - point.X))
                 {
-                    xDistance = Math.Abs(outlineRects[i].Width + outlineRects[i].X - point.X);
+                    xDistance = Math.Abs(outlineRects[i].Width + outlineRects[i].X - point.X) - buffer;
                 }
                 else
                 {
-                    xDistance = Math.Abs(outlineRects[i].X - point.X);
+                    xDistance = Math.Abs(outlineRects[i].X - point.X) - buffer;
                 }
                 if (Math.Abs(outlineRects[i].Height + outlineRects[i].Y - point.Y) < Math.Abs(outlineRects[i].Y - point.Y))
                 {
-                    yDistance = Math.Abs(outlineRects[i].Height + outlineRects[i].Y - point.Y);
+                    yDistance = Math.Abs(outlineRects[i].Height + outlineRects[i].Y - point.Y) - buffer;
                 }
                 else
                 {
-                    yDistance = Math.Abs(outlineRects[i].Y - point.Y);
+                    yDistance = Math.Abs(outlineRects[i].Y - point.Y) - buffer;
                 }
-                double distance = Math.Sqrt(Math.Pow(xDistance, 2) + Math.Pow(yDistance, 2));
-                if(distance > 30)
+                double distance = 40;
+                if(point.X <= outlineRects[i].Width + outlineRects[i].X + buffer && point.X >= outlineRects[i].X - buffer)
+                {
+                    distance = yDistance;
+                }
+                else if (point.Y >= outlineRects[i].Y - buffer && point.Y <= outlineRects[i].Height + outlineRects[i].Y + buffer)
+                {
+                    distance = xDistance;
+                }
+                else
+                {
+                    distance = Math.Sqrt(Math.Pow(xDistance, 2) + Math.Pow(yDistance, 2));
+                }
+                //double distance = Math.Sqrt(Math.Pow(xDistance, 2) + Math.Pow(yDistance, 2));
+                if (distance > 30)
                 {
                     currentAccuracy = 0.0;
                 }
                 else
                 {
-                    currentAccuracy = (40.0 - distance) / 30.0;
+                    currentAccuracy = (30.0 - distance) / 30.0;
                 }
                 if (currentAccuracy > bestAccuracy)
                     bestAccuracy = currentAccuracy;
