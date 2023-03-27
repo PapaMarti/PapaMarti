40e3c6fdac786d34abab0107b0bc034ebac820f7
Annabel Tu <170208@friscoisd.org>
2023-03-27 10:45:02 -0500

    1

---
+++ PapaMarti/PapaMarti/Room.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PapaMarti
+{
+    class Room
+    {
+        public Tile[,] tiles;
+        int height;
+        int width;
+        Vector2 origin;
+
+
+
+        readonly int SCREENWIDTH = 1920;
+        readonly int SCREENHEIGHT = 1080;
+
+        public Room(Tile[,] tiles_)
+        {
+            tiles = tiles_;
+            height = tiles.GetLength(0);
+            width = tiles.GetLength(1);
+
+            //Calculate origin at which to draw room
+            int pixelHeight = 60 * height;
+            int pixelWidth = 60 * width;
+            origin = new Vector2((SCREENWIDTH - pixelWidth) / 2, (SCREENHEIGHT - pixelHeight) / 2);
+        }
+        public void draw(SpriteBatch spriteBatch)
+        {
+            int x = (int)origin.X;
+            int y = (int)origin.Y;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    spriteBatch.Draw(tiles[i, j].texture, new Rectangle(x, y, 60, 60), Color.White);
+                    x += 60;
+                }
+                x = (int)origin.X;
+                y += 60;
+            }
+        }
+    }
+}
---
+++ PapaMarti/PapaMarti/Tile.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PapaMarti
+{
+    public enum Status
+    {
+        Passable, //Can walk through
+        Impassable, //Cannot walk through, walls or noninteractable objects
+        Collectable //Has an object that can be picked up
+    }
+    class Tile //Rooms will be made up of Tiles
+    {
+        public Status status;
+        public Texture2D texture;
+        Vector2 location;
+
+
+        //Null unless tile is Collectable
+        public Item item;
+
+        //Rooms will be displayed as a 2d array of tiles. Location refers to the row/column of the tile, not the actual coordinate on the screen.
+        //Tiles are 60 x 60
+        public Tile(Status status_, Texture2D texture_, Vector2 location_) //Non-collectable tiles
+        {
+            status = status_;
+            texture = texture_;
+            location = location_;
+            item = null;
+        }
+
+        public Tile(Status status_, Texture2D texture_, Vector2 location_, Item item_) : this(status_, texture_, location_)
+        {
+            item = item_;
+        }
+        
+        //Only needed for Collectable tiles
+        public Item collect()
+        {
+            status = Status.Passable;
+            Item itemHolder = item;
+            item = null;
+            return itemHolder;
+        }
+
+
+    }
+}
