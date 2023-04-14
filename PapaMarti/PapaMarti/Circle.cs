63f58f39bfff34127836305d2d1956fc4ea63e4c
Ella Whitney <ellawhitneyr@gmail.com>
2023-04-14 11:48:32 -0500

    circle class and weapon abstract class

---
+++ PapaMarti/PapaMarti/Circle.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PapaMarti
+{
+    public class Circle
+    {
+        public double angle;
+        public double radius;
+        public Vector2 location;
+
+        public Circle(double angle, double radius, Vector2 location)
+        {
+            this.angle = angle;
+            this.radius = radius;
+            this.location = location;
+        }
+
+        public bool isInCircle(Vector2 point)
+        {
+            return radius * radius > Math.Pow(point.X - location.X, 2) + Math.Pow(point.Y - location.Y, 2);
+        }
+    }
+}
---
+++ PapaMarti/PapaMarti/WeaponStuff/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PapaMarti.WeaponStuff
+{
+    public enum WeaponType
+    {
+        Throw,
+        Frisbee,
+        Bomb,
+        Sword,
+        SauceSprayer,
+        Cheeser,
+        Mace
+    }
+    public abstract class Weapon
+    {
+        public WeaponType weaponType;
+
+        //a way to calculate the area in which an enemy should be hurt by an attack
+        public Circle areaOfEffect;
+
+        public Weapon(WeaponType type)
+        {
+            weaponType = type;
+            areaOfEffect = null;
+        }
+
+        public abstract void draw(SpriteBatch spriteBatch);
+        public abstract void update(GameTime time);
+    }
+}
