2d1aeee36b3d78a6ce8bff4822ce616a3c73ab6e
Ella Whitney <ellawhitneyr@gmail.com>
2023-05-10 09:11:59 -0500

    changed rooms a bit

--- PapaMarti/PapaMarti/Game1.cs
+++ PapaMarti/PapaMarti/Game1.cs
@@ -33,7 +33,7 @@ namespace PapaMarti {
             Content.RootDirectory = "Content"; 
             graphics.PreferredBackBufferWidth = screenRect.Width;
             graphics.PreferredBackBufferHeight = screenRect.Height;
-            graphics.IsFullScreen = false;
+            graphics.IsFullScreen = true;
             IsMouseVisible = true;
             graphics.ApplyChanges();
         }
